696b6c91adcc792ec5db6d8cb62c7218be074089
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2026-01-09 22:58:02 +0100

    nix: add waydroid improvments

--- .nix/modules/nixos/android.nix
+++ .nix/modules/nixos/android.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 {
-  virtualisation.waydroid.enable = true;
+  virtualisation.waydroid = {
+    enable = true;
+    package = pkgs.waydroid-nftables;
+  };
+  systemd = {
+    packages = [ pkgs.waydroid-helper ];
+    services.waydroid-mount.wantedBy = [ "multi-user.target" ];
+  };
   programs = {
     adb.enable = true;
     kdeconnect.enable = true;
   };
   environment.systemPackages = with pkgs; [
     qtscrcpy
+    waydroid-helper
   ];
   users.users.eyouga.extraGroups = [
     "adbusers"
