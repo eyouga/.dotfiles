6e870cdcc235495c8d96bafc9ad13addf512cf21
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2026-02-04 09:32:20 +0100

    nix (trueman): stub init

---
+++ .nix/hosts/trueman/configuration.nix
@@ -0,0 +1,86 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    inputs.disko.nixosModules.disko
+    ./disko-config.nix
+    ../../modules/user.nix
+    ../../modules/packages.nix
+    ../../modules/nixos/audio.nix
+    ../../modules/nixos/games.nix
+    ../../modules/nixos/locale.nix
+    ../../modules/nixos/waydroid.nix
+    ../../modules/nixos/packages.nix
+    ../../modules/nixos/maintenance.nix
+    ../../modules/nixos/configuration.nix
+    ../../modules/wm/plasma.nix
+  ];
+
+  networking.hostName = "biggie";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  system.stateVersion = "25.05";
+
+  boot = {
+    blacklistedKernelModules = [
+      "ohci1394"
+      "raw1394"
+      "video1394"
+      "sbp2"
+      # "snd-dice"
+    ];
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+      ];
+    };
+    kernelModules = {
+      kvm-amd = true;
+    };
+    extraModulePackages = [ ];
+  };
+
+  hardware = {
+    i2c.enable = true;
+    bluetooth = {
+      enable = true; # enables support for Bluetooth
+      powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    };
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+
+  systemd.services.firewire-modules = {
+    description = "Load FireWire modules late so the soundcard gets detected";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-udev-settle.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = [
+        "${pkgs.kmod}/bin/modprobe firewire-ohci"
+        "${pkgs.kmod}/bin/modprobe firewire-core"
+        "${pkgs.kmod}/bin/modprobe firewire-sbp2"
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    easyeffects
+  ];
+
+  users.users.eyouga.extraGroups = [
+    "networkmanager"
+    "i2c"
+    "adbusers"
+  ];
+}
