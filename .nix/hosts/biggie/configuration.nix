8764be345351158dd23f8dfc46ef9fe3c4c2842b
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2026-05-26 15:48:45 +0200

    nix (biggie): add OBS

--- .nix/hosts/biggie/configuration.nix
+++ .nix/hosts/biggie/configuration.nix
@@ -76,6 +76,18 @@
     };
   };
 
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     easyeffects
   ];
