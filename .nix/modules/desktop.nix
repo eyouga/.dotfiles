e4832d6493c91fa4b15ea6b63533b40cbaca13f0
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2026-06-29 00:35:37 +0200

    nix: cleanup

--- .nix/modules/desktop.nix
+++ .nix/modules/desktop.nix
@@ -8,8 +8,6 @@
   imports = [
     ./configuration.nix
   ];
-  programs.nix-index.enable = true;
-  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     android-tools
     inputs.fjordlauncher.packages."${pkgs.stdenv.hostPlatform.system}".fjordlauncher
@@ -17,12 +15,5 @@
     localsend
     scrcpy
     yt-dlp
-  ];
-
-  # add nerd fonts
-  fonts.packages = with pkgs; [
-    nerd-fonts.droid-sans-mono
-    nerd-fonts.hack
-    nerd-fonts.iosevka
   ];
 }
